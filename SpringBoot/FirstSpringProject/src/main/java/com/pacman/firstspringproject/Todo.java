9634ed36e7824f93b3e387bb5c23defdfb85ef13
Puspo <puspo@pacman.local>
2025-08-11 00:17:26 +0600

    Added first basic model

---
+++ SpringBoot/FirstSpringProject/src/main/java/com/pacman/firstspringproject/Todo.java
@@ -0,0 +1,4 @@
+package com.pacman.firstspringproject;
+
+public class Todo {
+}
---
+++ SpringBoot/UberSpringProject/UberReviewService/src/main/java/com/pacman/uberreviewservice/models/Review.java
@@ -0,0 +1,12 @@
+package com.pacman.uberreviewservice.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Review {
+    @Id
+    Integer id;
+}
