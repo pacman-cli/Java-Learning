ef31d5cbd6bcd334f32f0695882edcdfd7f3abf8
Puspo <puspo@pacman.local>
2025-08-20 21:23:11 +0600

    added Booking and Review Adapter

---
+++ SpringBoot/UberSpringProject/UberReviewService/src/main/java/com/pacman/uberreviewservice/adapter/BookingAdapter.java
@@ -0,0 +1,57 @@
+package com.pacman.uberreviewservice.adapter;
+
+import com.pacman.uberreviewservice.dtos.BookingDto;
+import com.pacman.uberreviewservice.models.Booking;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
+
+//toBookingDto,toEntity
+@Component
+public class BookingAdapter {
+    public BookingDto toBookingDto(Booking booking) {
+        if (booking == null) {
+            return null;
+        }
+        return BookingDto.builder()
+                .id(booking.getId())
+                .bookingStatus(booking.getBookingStatus())
+                .startTime(convertToLocalDateTime(booking.getStartTime()))
+                .endTime(convertToLocalDateTime(booking.getEndTime()))
+                .totalTime(booking.getTotalTime())
+                .driverId(booking.getDriver() != null ? booking.getDriver().getId() : null)
+                .passengerId(booking.getPassenger() != null ? booking.getPassenger().getId() : null)
+                .createdAt(convertToLocalDateTime(booking.getCreatedAt()))
+                .updatedAt(convertToLocalDateTime(booking.getUpdatedAt()))
+                .build();
+    }
+
+    public Booking toEntity(BookingDto bookingDto) {
+        if (bookingDto == null) {
+            return null;
+        }
+        return Booking.builder()
+                .bookingStatus(bookingDto.getBookingStatus())
+                .startTime(convertToLocalDate(bookingDto.getStartTime()))
+                .endTime(convertToLocalDate(bookingDto.getEndTime()))
+                .totalTime(bookingDto.getTotalTime())
+                .build();
+    }
+
+    private Date convertToLocalDate(LocalDateTime localDateTime) {
+        if (localDateTime == null) {
+            return null;
+        }
+        return Date.from(localDateTime.atZone(ZoneId.systemDefault()).toInstant());
+    }
+
+    private LocalDateTime convertToLocalDateTime(Date date) {
+        if (date == null) {
+            return null;
+        }
+        return date.toInstant().atZone(java.time.ZoneId.systemDefault()).toLocalDateTime();
+    }
+
+}
--- SpringBoot/UberSpringProject/UberReviewService/src/main/resources/db/migration/V2__add_phonenumber_to_driver.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE uber_db_local.driver
-    ADD phone_number VARCHAR(255) NULL;
