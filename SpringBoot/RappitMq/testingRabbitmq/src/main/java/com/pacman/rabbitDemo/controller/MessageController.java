5f908a2835ad25822f03434575d70b5e02a85da3
pacman-cli <puspopuspo520@gmail.com>
2025-09-11 13:47:43 +0600

    Added Sending → via REST API → RabbitMQ → Consumer → DB + WebSocket.

--- SpringBoot/RappitMq/testingRabbitmq/src/main/java/com/pacman/rabbitDemo/controller/MessageController.java
+++ SpringBoot/RappitMq/testingRabbitmq/src/main/java/com/pacman/rabbitDemo/controller/MessageController.java
@@ -1,8 +1,12 @@
 package com.pacman.rabbitDemo.controller;
 
+import com.pacman.rabbitDemo.entity.ReceivedMessage;
+import com.pacman.rabbitDemo.repository.ReceivedMessageRepository;
 import com.pacman.rabbitDemo.services.MessageProducer;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 //@CrossOrigin(origins = "http://localhost:3000") --> if you want to allow cross-origin requests can use this or
 // CorsConfig(WebMvcConfigurer)
@@ -11,10 +15,20 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class MessageController {
     private final MessageProducer messageProducer;
+    private final ReceivedMessageRepository receivedMessageRepository;
 
     @PostMapping
     public String sendMessage(@RequestParam String msg){
-        messageProducer.sendMessage(msg);
+        messageProducer.sendMessage(msg); //must go to rabbit
         return "Sent message: " + msg;
+    }
+
+//    @GetMapping
+//    public Iterable<?> getAllMessages(){
+//        return receivedMessageRepository.findAll();
+//    }
+    @GetMapping("/all")
+    public List<ReceivedMessage> getAllMessages(){
+        return receivedMessageRepository.findAll();
     }
 }
--- SpringBoot/RappitMq/testingRabbitmq/src/main/java/com/pacman/rabbitDemo/services/MessageConsumer.java
+++ SpringBoot/RappitMq/testingRabbitmq/src/main/java/com/pacman/rabbitDemo/services/MessageConsumer.java
@@ -6,6 +6,7 @@ import com.pacman.rabbitDemo.repository.ReceivedMessageRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -15,19 +16,26 @@ import java.time.LocalDateTime;
 public class MessageConsumer {
     private final MemoryStorage memoryStorage;
     private final ReceivedMessageRepository receivedMessageRepository;
+    private final SimpMessagingTemplate brokerMessagingTemplate;
     @Value("${app.rabbitmq.queue}")
     private String queue;
 
     @RabbitListener(queues = "#{queue.name}")
     public void receiveMessage(String message){
         System.out.println("Received message: " + message);
-        memoryStorage.addMessage(message);
 
-        //saving to DB with builder pattern
+
+        //saving to DB with a builder pattern
         ReceivedMessage saved = ReceivedMessage.builder()
                 .content(message)
                 .createdAt(LocalDateTime.now())
                 .build();
         receivedMessageRepository.save(saved);
+
+        //Store in memory
+        memoryStorage.addMessage(message);
+
+        //this will send a message to a client via websocket
+        brokerMessagingTemplate.convertAndSend("/topic/messages",message);
     }
 }
