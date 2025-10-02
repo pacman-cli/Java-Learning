e3fa61830a013951b7b74741fc5b6639c4bca4e9
pacman-cli <puspopuspo520@gmail.com>
2025-10-02 08:50:23 +0600

    this is rabbitmq mini project with json data converter and setting up with(queue,exchange,routing key) and binding queue to routing key

---
+++ RabbitMQ/rabbitMqDemo/src/main/java/com/pacman/rabbitmqdemo/config/RabbitMQConfig.java
@@ -0,0 +1,73 @@
+package com.pacman.rabbitmqdemo.config;
+
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+    @Value( "${rabbitmq.queue.name}")
+    private String queue;
+    @Value( "${rabbitmq.exchange.name}")
+    private String exchange;
+    @Value( "${rabbitmq.routing.key}")
+    private String routingKey;
+    @Value( "${rabbitmq.routing.json.key}")
+    private String routingJsonKey;
+    @Value( "${rabbitmq.json.queue.name}")
+    private String jsonQueue;
+    //spring bean for rabbitmq queue
+    @Bean
+    public Queue queue() {
+        return new Queue(queue);
+    }
+    //spring bean for rabbitmq JSON queue
+    @Bean
+    public Queue jsonQueue() {
+        return new Queue(jsonQueue);
+    }
+    //spring bean for rabbitmq exchange
+    @Bean
+    public TopicExchange exchange(){
+        return new TopicExchange(exchange);
+    }
+    //now we have to bind queue and exchange with a routing key
+    @Bean
+    public Binding binding(){
+        return BindingBuilder
+                .bind(queue())
+                .to(exchange())
+                .with(routingKey);
+    }
+
+    //now we have to bind queue and exchange with a routing key
+    @Bean
+    public Binding bindingJson(){
+        return BindingBuilder
+                .bind(jsonQueue())
+                .to(exchange())
+                .with(routingJsonKey);
+    }
+    //Message converter bean
+    @Bean
+    public MessageConverter messageConverter(){
+        return new Jackson2JsonMessageConverter();
+    }
+
+    //connection factory bean
+    //rabbit template bean (now this has the json message support because we've set the message converter to rabbit template)
+    @Bean
+    public AmqpTemplate rabbitTemplate(ConnectionFactory connectionFactory){
+        RabbitTemplate rabbitTemplate = new RabbitTemplate( connectionFactory);
+        rabbitTemplate.setMessageConverter(messageConverter());
+        return rabbitTemplate;
+    }
+    //rabbit admin bean
+    //this three beans are required for rabbitmq and will be auto configured by spring boot
+
+}
