b4e6772c93e0722a375fcf2df5b49e26d71eee33
pacman-cli <puspopuspo520@gmail.com>
2025-10-11 11:46:49 +0600

    added a file uploading endpoint with cloudinary(public)

---
+++ SpringBoot/Cloudinary/videoshare/src/main/java/com/puspo/scalablekafkaapp/videoshare/model/Image.java
@@ -0,0 +1,26 @@
+package com.puspo.scalablekafkaapp.videoshare.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Image {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String publicId;
+    private String url;
+    private String name;
+}
